ac56602c705e562031dc8d476449c919ad7cb454
christianpiccoli <christianpiccoli@hotmail.it>
2023-03-21 13:34:25 +0100

    Test commit to check .idea folder was excluded

--- src/main/java/it/polimi/App.java
+++ src/main/java/it/polimi/App.java
@@ -6,7 +6,7 @@ package it.polimi;
  */
 public class App 
 {
-    //test commit;
+    //test commit
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
