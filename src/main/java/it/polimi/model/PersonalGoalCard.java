ff8c81a03c130e51971279b6a721d1c47872ed89
cpiccoli-polimi <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-03-23 18:42:18 +0100

    Update PersonalGoalCard.java
    Removed "abstract"

--- src/main/java/it/polimi/model/PersonalGoalCard.java
+++ src/main/java/it/polimi/model/PersonalGoalCard.java
@@ -1,6 +1,6 @@
 package it.polimi.model;
 
-public abstract class PersonalGoalCard extends GoalCard{
+public class PersonalGoalCard extends GoalCard{
     private static Bookshelf goal;
     private static Map<int,int> points;
 }
