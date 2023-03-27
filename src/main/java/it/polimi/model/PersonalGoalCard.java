56b9b625cbc5c8dab33840a8e0f034f13c4de1e7
cpiccoli-polimi <christian.piccoli@mail.polimi.it>
2023-03-27 14:22:29 +0200

    Added PersonalGoalCard constructor (still to be finished) Added test json file that will cointan Personal Goal Cards Modified pom.xml to use json-simple, needed to read from JSON file

--- src/main/java/it/polimi/model/PersonalGoalCard.java
+++ src/main/java/it/polimi/model/PersonalGoalCard.java
@@ -1,6 +1,33 @@
 package it.polimi.model;
 
+import java.io.FileReader;
+import java.util.Map;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
+
 public class PersonalGoalCard extends GoalCard{
     private static Bookshelf goal;
-    private static Map<int,int> points;
+    private static Map<Integer,Integer> points;
+    public PersonalGoalCard(int id) {
+        // TODO: Sistemare lettura da file JSON
+            /*JSONParser jsonParser = new JSONParser();
+
+            try (FileReader reader = new FileReader("PersonalGoalCards.json"))
+            {
+                JSONObject obj  = (JSONObject)obj;
+                String name = (String)obj.get("Name");
+                String course = (String)obj.get("Course");
+                JSONArray subjects = (JSONArray)obj.get("Subjects");
+          }*/
+        // TODO: Inizializzazione corretta?
+        points =  Map.ofEntries(
+                Map.entry(1, 1),
+                Map.entry(2, 2),
+                Map.entry(3, 4),
+                Map.entry(5, 9),
+                Map.entry(6, 12)
+        );
+        };
 }
+
