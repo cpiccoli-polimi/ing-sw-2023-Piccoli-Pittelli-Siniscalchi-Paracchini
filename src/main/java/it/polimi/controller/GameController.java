56049d52a7963cec0ce81c12c8be7959a4919fa7
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-03-31 17:05:39 +0200

    Created controller package and GameController class

---
+++ src/main/java/it/polimi/controller/GameController.java
@@ -0,0 +1,8 @@
+package it.polimi.controller;
+import it.polimi.model.Game;
+public class GameController {
+    public final Game model;
+    public GameController(Game model){
+        this.model=model;
+    }
+}
