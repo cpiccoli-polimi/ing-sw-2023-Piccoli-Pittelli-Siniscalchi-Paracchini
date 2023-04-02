e1ae5536a7ed4bba143ebb028311e3a20f428753
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-04-02 09:57:28 +0200

    Game.java update according to classDiagram's last changes
    addition of a custom class constructor
    removal of pointsDeck attribute and its setter and getter methods
    removal of the following methods: setPlayersNumber, setCommonGoalsNumber, setBoard, setBag,  setPersonalGoalsDeck, setCommonGoalsDeck
    addition of endGamePoints attribute and its getter method
    addition of the following methods: nextTurn, updateCommonGoals, updateBoard

--- src/main/java/it/polimi/model/Game.java
+++ src/main/java/it/polimi/model/Game.java
@@ -6,6 +6,7 @@ public class Game extends Observable{
 
     private int id;
     private boolean done;
+    private int endGamePoints;
     private int playersNumber;
     private int commonGoalsNumber;
     private Player[] table;
@@ -13,10 +14,10 @@ public class Game extends Observable{
     private CardsBag bag;
     private PersonalGoalCard[] personalGoalsDeck;
     private CommonGoalCard[] commonGoalsDeck;
-    private PointCard[] pointsDeck;
     private int currentPlayer;
     private Player[] leaderboard;
 
+    public Game(int playersNumber, int commonGoalsNumber){    }
     public int getId(){
         return id;
     }
@@ -26,88 +27,49 @@ public class Game extends Observable{
     public boolean getDone() {
         return done;
     }
-
     public void setDone(boolean done) {
         this.done = done;
     }
-
+    public int getEndGamePoints() {
+        return endGamePoints;
+    }
     public int getPlayersNumber(){
         return playersNumber;
     }
-
-    public void setPlayersNumber(int playersNumber) {
-        this.playersNumber = playersNumber;
-    }
-
     public int getCommonGoalsNumber(){
         return commonGoalsNumber;
     }
-    public void setCommonGoalsNumber(int commonGoalsNumber) {
-        this.commonGoalsNumber = commonGoalsNumber;
-    }
-
     public Player[] getTable() {
         return table;
     }
-
     public void setTable(Player[] table) {
         this.table = table;
     }
-
     public LivingRoomBoard getBoard() {
         return board;
     }
-
-    public void setBoard(LivingRoomBoard board) {
-        this.board = board;
-    }
-
     public CardsBag getBag() {
         return bag;
     }
-
-    public void setBag(CardsBag bag) {
-        this.bag = bag;
-    }
-
     public PersonalGoalCard[] getPersonalGoalsDeck() {
         return personalGoalsDeck;
     }
-
-    public void setPersonalGoalsDeck(PersonalGoalCard[] personalGoalsDeck) {
-        this.personalGoalsDeck = personalGoalsDeck;
-    }
-
     public CommonGoalCard[] getCommonGoalsDeck() {
         return commonGoalsDeck;
     }
-
-    public void setCommonGoalsDeck(CommonGoalCard[] commonGoalsDeck) {
-        this.commonGoalsDeck = commonGoalsDeck;
-    }
-
-    public PointCard[] getPointsDeck() {
-        return pointsDeck;
-    }
-
-    public void setPointsDeck(PointCard[] pointsDeck) {
-        this.pointsDeck = pointsDeck;
-    }
-
     public int getCurrentPlayer(){
         return currentPlayer;
     }
-
     public void setCurrentPlayer(int currentPlayer) {
         this.currentPlayer = currentPlayer;
     }
-
     public Player[] getLeaderboard(){
         return leaderboard;
     }
-
     public void setLeaderboard(Player[] leaderboard) {
         this.leaderboard = leaderboard;
     }
-
+    public void nextTurn(){    }
+    public void updateCommonGoals(CommonGoalCard commonGoal){    }
+    public void updateBoard(){    }
 }
