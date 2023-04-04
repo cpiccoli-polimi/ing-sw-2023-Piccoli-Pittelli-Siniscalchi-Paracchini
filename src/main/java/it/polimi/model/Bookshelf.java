b8e2157a6f6f58c5669ad234a44ede3ff2006c71
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-04-04 14:55:28 +0200

    Modified bookshelf size and its initialization

--- src/main/java/it/polimi/model/Bookshelf.java
+++ src/main/java/it/polimi/model/Bookshelf.java
@@ -5,7 +5,12 @@ public class Bookshelf {
     protected ObjectCard [][] shelf;
     protected Bookshelf(){
         this.maxDrawableObjects=3;
-        this.shelf= new ObjectCard[5][6];
+        this.shelf= new ObjectCard[6][5];
+        for(int i=0;i<shelf.length;i++){
+            for(int j=0;j<shelf[0].length;j++){
+                shelf[i][j]=null;
+            }
+        }
     }
     public void setMaxDrawableObjects(int maxDrawableObjects) {this.maxDrawableObjects = maxDrawableObjects;}
     public int getMaxDrawableObjects(){return maxDrawableObjects;}
