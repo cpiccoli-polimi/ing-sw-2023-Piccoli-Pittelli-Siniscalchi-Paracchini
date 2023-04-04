e3a6a5f1de70aa4bfb1a09b84512672909c6f3c4
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-04-04 11:35:51 +0200

    CommonGoalCard10.java check method addition

--- src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard10.java
+++ src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard10.java
@@ -2,11 +2,26 @@ package it.polimi.model.CommonGoalCards;
 
 import it.polimi.model.CommonGoalCard;
 import it.polimi.model.PointCard;
+import it.polimi.model.Type;
 
 public class CommonGoalCard10 extends CommonGoalCard {
     public CommonGoalCard10(int playersNumber) {
         goalID = 10;
         // Deck creation
         points = new PointCard[playersNumber];
+    }
+
+    public boolean check(ObjectCard[][] bookshelf) {
+        boolean flag = false;
+
+        for(int i = 1; i < bookshelf.length - 1 && flag == false; i++){
+            for(int j = 1; j < bookshelf[i].length - 1 && flag == false; j++){
+                if(bookshelf[i-1][j-1].getType() == bookshelf[i-1][j+1].getType() == bookshelf[i][j].getType() == bookshelf[i+1][j-1].getType() == bookshelf[i+1][j+1].getType()){
+                    flag = true;
+                }
+            }
+        }
+
+        return flag;
     }
 }
