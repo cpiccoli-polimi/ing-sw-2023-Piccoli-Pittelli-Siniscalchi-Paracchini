8787b15e2099e42bbedfe51f52efbec0580e0c04
lorblock <128162349+lorblock@users.noreply.github.com>
2023-04-13 04:00:04 +0200

    CommonGoalCard4.java check method addition

--- src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard4.java
+++ src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard4.java
@@ -4,9 +4,38 @@ import it.polimi.model.CommonGoalCard;
 import it.polimi.model.PointCard;
 
 public class CommonGoalCard4 extends CommonGoalCard {
+
     public CommonGoalCard4(int playersNumber) {
         goalID = 4;
         // Deck creation
         points = new PointCard[playersNumber];
+    }
+
+    public boolean check(ObjectCard[][] bookshelf) {
+        List<Type> typeList= new ArrayList<Type>();
+        int rowsCounter = 0;
+
+        for (int i = 0; i < bookshelf[0].length; i++) {
+            for (int j = 0; j < bookshelf.length; j++) {
+
+                if (bookshelf[j][i].getType() != null) {
+
+                    if (!typeList.contains(bookshelf[j][i].getType())) {
+                        typeList.add(bookshelf[j][i].getType());
+                    }
+                }else {
+                    continue;
+                }
+            }
+
+            if (typeList.size() <= 3) {
+                rowsCounter += 1;
+            }
+
+            typeList.clear();
+        }
+
+        if (rowsCounter < 4){ return false; }
+        else { return true; }
     }
 }
