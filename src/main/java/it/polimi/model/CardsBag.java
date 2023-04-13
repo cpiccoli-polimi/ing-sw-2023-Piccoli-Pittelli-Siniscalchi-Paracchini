7e476e86fb7c2893051ed255e63f5655dab04a04
lorblock <128162349+lorblock@users.noreply.github.com>
2023-04-13 02:48:16 +0200

    Added CardsBag.java

---
+++ src/main/java/it/polimi/model/CardsBag.java
@@ -0,0 +1,44 @@
+package it.polimi.model;
+
+import java.lang.Object;
+import java.time.LocalTime;
+import java.util.Random;
+import static java.util.Collections.shuffle;
+
+public class CardsBag {
+    private int[] cards;
+
+    protected void CardsBag() {
+        LocalTime clock = LocalTime.now();
+        int hours = clock.getHour();
+        int minutes = clock.getMinute();
+        int seconds = clock.getSecond();
+        String clockString = new String();
+        clockString += hours;
+        clockString += minutes;
+        clockString += seconds;
+        long seed = parseLong(clockString);
+        Random generator = new Random(seed);
+
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < 132; i++) {
+            list.add(i+1);
+        }
+
+        shuffle(list, generator);
+
+        for(int i = 0; i < 132; i++) {
+            cards[i] = list.get(i);
+        }
+    };
+
+    public int getCard(){
+        id = cards[0];
+        cards = ArrayUtils.removeElement(cards, id)
+        return id;
+    };
+
+    private void setCards(int[] cards){
+        this.cards = cards;
+    };
+}
