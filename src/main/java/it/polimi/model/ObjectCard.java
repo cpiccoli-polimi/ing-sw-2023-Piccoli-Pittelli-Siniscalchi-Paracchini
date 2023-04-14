6b7c1d0209381d1ffe5bae74fd3e2fe71fb9dd18
lorblock <128162349+lorblock@users.noreply.github.com>
2023-04-14 16:28:02 +0200

    Update ObjectCard.java

--- src/main/java/it/polimi/model/ObjectCard.java
+++ src/main/java/it/polimi/model/ObjectCard.java
@@ -11,17 +11,17 @@ public class ObjectCard {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
 
-        if (id < 5) {
+        if (id < 23) {
             this.type = Type.Cats;
-        }else if (5 < id && id < 10) {
+        }else if (23 >= id && id < 45) {
             this.type = Type.Trophies;
-        }else if (10 < id && id < 15) {
+        }else if (45 >= id && id < 67) {
             this.type = Type.Plants;
-        }else if (15 < id && id < 20) {
+        }else if (67 >= id && id < 89) {
             this.type = Type.Books;
-        }else if (20 < id && id < 25) {
+        }else if (89 >= id && id < 101) {
             this.type = Type.Frames;
-        }else if (25 < id && id < 30) {
+        }else if (101 >= id && id < 123) {
             this.type = Type.Games;
         }
     };
