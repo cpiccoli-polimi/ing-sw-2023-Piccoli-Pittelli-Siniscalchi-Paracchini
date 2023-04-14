9a7110b08ddee002bd6eb05fcea6d9b265714b55
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-04-14 21:29:45 +0200

    Method correction in CommonGoalCard.java
    addition of all the code for the setPoints() method

--- src/main/java/it/polimi/model/CommonGoalCard.java
+++ src/main/java/it/polimi/model/CommonGoalCard.java
@@ -4,6 +4,8 @@ public abstract class CommonGoalCard extends GoalCard{
 
     protected int goalID;
     protected PointCard[] points;
-    protected void setPoints(PointCard[] points){}
+    protected void setPoints(PointCard[] points){
+        this.points = points;
+    }
     public boolean check(ObjectCard[][] bookshelf){return false;}
 }
