1eb2725cc0b796cd6f2afa6682246c7ca21ede66
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-04-14 08:23:11 +0200

    Addition of the ObjectCard import statement to CommonGoalCard8.java, CommonGoalCard10.java and CommonGoalCard12.java

--- src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard12.java
+++ src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard12.java
@@ -1,6 +1,7 @@
 package it.polimi.model.CommonGoalCards;
 
 import it.polimi.model.CommonGoalCard;
+import it.polimi.model.ObjectCard;
 import it.polimi.model.PointCard;
 
 public class CommonGoalCard12 extends CommonGoalCard {
