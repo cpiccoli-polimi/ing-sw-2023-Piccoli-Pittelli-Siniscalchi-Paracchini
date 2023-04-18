a783151cf4ce4d4b4801a400b3cc8cf399c22326
Christian Piccoli <christian.piccoli@mail.polimi.it>
2023-04-18 16:26:55 +0200

    Created CommonGoalCard3Test

--- src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard3Test.java
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard3Test.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CommonGoalCard4Test {
+class CommonGoalCard3Test {
     @Test
-    void CommonGoal4SatisfiedTest(){
+    void CommonGoal3SatisfiedTest(){
         PublicObjectCard[][] bookshelf;
         bookshelf= new PublicObjectCard[6][5];
-        CommonGoalCard4 cm4=new CommonGoalCard4(4);
+        CommonGoalCard3 cm3=new CommonGoalCard3(4);
         //Same type card on the four corners
         bookshelf[0][0]=new PublicObjectCard(1,0,0);
         bookshelf[5][0]=new PublicObjectCard(1,5,0);
         bookshelf[5][4]=new PublicObjectCard(1,5,4);
         bookshelf[0][4]=new PublicObjectCard(1,0,4);
 
-        assertTrue(cm4.check(bookshelf));
+        assertTrue(cm3.check(bookshelf));
     }
 }
