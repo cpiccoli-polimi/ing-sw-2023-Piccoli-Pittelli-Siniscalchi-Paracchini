4ab440c9e33d5b03edbda5701304f9d933a8025a
Christian Piccoli <christian.piccoli@mail.polimi.it>
2023-04-18 16:32:43 +0200

    Created CommonGoalCard5Test

---
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard5Test.java
@@ -0,0 +1,37 @@
+package it.polimi.model.CommonGoalCards;
+
+import it.polimi.model.PublicObjectCard;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonGoalCard5Test {
+    @Test
+    void CommonGoal5SatisfiedTest(){
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard5 cm5=new CommonGoalCard5(4);
+        //4 groups of 4 cards with the same type in line
+        bookshelf[0][0]=new PublicObjectCard(1,0,0);
+        bookshelf[0][1]=new PublicObjectCard(1,0,1);
+        bookshelf[0][2]=new PublicObjectCard(1,0,2);
+        bookshelf[0][3]=new PublicObjectCard(1,0,3);
+
+        bookshelf[1][1]=new PublicObjectCard(1,1,1);
+        bookshelf[1][2]=new PublicObjectCard(1,1,2);
+        bookshelf[1][3]=new PublicObjectCard(1,1,3);
+        bookshelf[1][4]=new PublicObjectCard(1,1,4);
+
+        bookshelf[1][0]=new PublicObjectCard(1,1,0);
+        bookshelf[2][0]=new PublicObjectCard(2,1,0);
+        bookshelf[3][0]=new PublicObjectCard(3,1,0);
+        bookshelf[4][0]=new PublicObjectCard(4,1,0);
+
+        bookshelf[4][1]=new PublicObjectCard(1,4,1);
+        bookshelf[4][2]=new PublicObjectCard(1,4,2);
+        bookshelf[4][3]=new PublicObjectCard(1,4,3);
+        bookshelf[4][4]=new PublicObjectCard(1,4,4);
+
+        assertTrue(cm5.check(bookshelf));
+    }
+}
