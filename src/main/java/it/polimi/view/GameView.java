a4a484ec1094979dbe52b273b25f38e73e0d3684
Christian Piccoli <christian.piccoli@mail.polimi.it>
2023-04-24 15:54:25 +0200

    Upload of CommonGoals.json with all common goals' descriptions

--- src/main/java/it/polimi/view/GameView.java
+++ src/main/java/it/polimi/view/GameView.java
@@ -1,4 +1,14 @@
 package it.polimi.view;
 
+import it.polimi.controller.GameController;
+
+import java.util.Scanner;
+
 public class GameView {
+    String askNickname(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter nickname");
+        String nickname = userInput.nextLine();
+        int commonGoalsNumber = GameController.get
+    }
 }
