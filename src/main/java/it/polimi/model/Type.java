92022376079e4568b5834adcdc46a508d298133b
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-04-25 22:26:15 +0200

    Type.java variables addition
    color and RESET variable addition to Type.java
    getColor() method addition to Type.java
    custom constructor addition to Type.java

--- src/main/java/it/polimi/model/Type.java
+++ src/main/java/it/polimi/model/Type.java
@@ -1,10 +1,21 @@
 package it.polimi.model;
 
 public enum Type {
-    Cats,
-    Books,
-    Games,
-    Frames,
-    Trophies,
-    Plants;
+    Cats("\u001B[32m"),
+    Books("\u001B[37m"),
+    Games("\u001B[33m"),
+    Frames("\u001B[34m"),
+    Trophies("\u001B[36m"),
+    Plants("\u001B[35m");
+
+    static final String RESET = "\u001B[0m";
+
+    private String color;
+
+    Type(String color) {
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
 }
