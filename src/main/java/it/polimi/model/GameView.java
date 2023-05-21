23a138ebf5890a6a1352b6e46e9fd917e246c32e
Christian Piccoli <christian.piccoli@mail.polimi.it>
2023-05-21 11:27:47 +0200

    Completed GameView class in Model with all attributes and methods

--- src/main/java/it/polimi/model/GameView.java
+++ src/main/java/it/polimi/model/GameView.java
@@ -2,15 +2,21 @@ package it.polimi.model;
 
 public class GameView {
     private long serialVersionUID;
+    private Player[] table;
+    private Player[] leaderboard;
+    private LivingRoomBoard board;
 
     public GameView(long serialVersionUID) {
         this.serialVersionUID = serialVersionUID;
     }
 
-    public void PlayerChoice(Game game){};
-    public Player[] getTable(){};
-    public Player[] getLeaderboard(){};
-    public LivingRoomBoard getBoard(){};
-
+    public void PlayerChoice(Game game){
+        this.table = game.getTable();
+        this.leaderboard = game.getLeaderboard();
+        this.board = game.getBoard();
+    }
+    public Player[] getTable(){return table;}
+    public Player[] getLeaderboard(){return leaderboard;}
+    public LivingRoomBoard getBoard(){return board;}
 
 }
