1b91c09d529584ea720847434b2ce16d1605549e
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-05-21 23:02:29 +0200

    Added update(GameView) and MessageReceiver

--- src/main/java/it/polimi/view/RemoteView.java
+++ src/main/java/it/polimi/view/RemoteView.java
@@ -1,18 +1,32 @@
-
 package it.polimi.view;
-
-
-        import it.polimi.model.Player;
-        import it.polimi.observer.Observer;
-
-        import java.util.Observable;
-
+import it.polimi.model.GameView;
+import it.polimi.model.Player;
+import it.polimi.observer.Observer;
 
 public class RemoteView extends View {
     private ClientConnection clientConnection;
+    private Player player;
+    public RemoteView(Player player, ClientConnection c){
+        super(player);
+        this.clientConnection=c;
+        c.addObserver(new MessageReceiver());
+
+    }
+    @Override
+    protected void showMessage(Object message){ clientConnection.asyncSend(message);}
+
 
     @Override
     public void update(GameView message) {
+        System.out.println("received: "+message);
+        try{
+            clientConnection.asyncSend(message);
+            if(message.getCurrPlayer()==player.getPosition()){
+                clientConnection.asyncSend(message.getMessage());
+            }
+        }catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e){
+            clientConnection.asyncSend("Error!");
+        }
 
     }
 
@@ -29,14 +43,4 @@ public class RemoteView extends View {
 
         }
     }
-    public RemoteView(Player player, ClientConnection c){
-        super(player);
-        this.clientConnection=c;
-        c.addObserver(new MessageReceiver());
-
-    }
-    @Override
-    protected void showMessage(Object message){ clientConnection.asyncSend(message);}
-
-
 }
