423fecce9d95de4c09503bc3b57066a061a35362
NicolaSiniscalchi <nicola.siniscalchi@mail.polimi.it>
2023-05-30 14:44:16 +0200

    NotYourTurnException.java addition to controller\exception package

---
+++ src/main/java/it/polimi/controller/exception/NotYourTurnException.java
@@ -0,0 +1,6 @@
+package it.polimi.controller.exception;
+
+public class NotYourTurnException extends Exception{
+    public NotYourTurnException(){super();}
+    public NotYourTurnException(String s){super(s);}
+}
