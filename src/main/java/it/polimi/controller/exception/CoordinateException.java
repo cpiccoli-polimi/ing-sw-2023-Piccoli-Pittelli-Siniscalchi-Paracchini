193157b03cfb57353c6d7e863c36c25e480854c3
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-14 22:06:20 +0200

    Added CoordinateException

---
+++ src/main/java/it/polimi/controller/exception/CoordinateException.java
@@ -0,0 +1,6 @@
+package it.polimi.controller.exception;
+
+public class CoordinateException extends Exception{
+    public CoordinateException(){super();}
+    public CoordinateException(String s){super(s);}
+}
