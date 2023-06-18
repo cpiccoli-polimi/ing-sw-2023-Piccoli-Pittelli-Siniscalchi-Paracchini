67a9a96f3eab83f4d5cad0aaa789fe0d09558d43
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-18 23:32:29 +0200

    Now OutOfBookshelfException extends Exception

--- src/main/java/it/polimi/controller/exception/OutOfBookshelfException.java
+++ src/main/java/it/polimi/controller/exception/OutOfBookshelfException.java
@@ -1,9 +1,6 @@
 package it.polimi.controller.exception;
 
-public class OutOfBookshelfException extends IndexOutOfBoundsException {
-    @Override
-    public String getMessage(){
-        return "In which bookshelf column do you want to insert those cards?";
-    }
+public class OutOfBookshelfException extends Exception {
+    public OutOfBookshelfException(){super();}
+    public OutOfBookshelfException(String s){super(s);}
 }
-
