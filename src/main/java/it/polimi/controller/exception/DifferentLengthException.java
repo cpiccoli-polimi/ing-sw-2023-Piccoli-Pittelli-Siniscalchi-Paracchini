5b30f7e056df2ed4310f9ef29e30886d73303980
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-22 18:11:46 +0200

    Implemented DifferentLengthException and OrderException

---
+++ src/main/java/it/polimi/controller/exception/DifferentLengthException.java
@@ -0,0 +1,5 @@
+package it.polimi.controller.exception;
+
+public class DifferentLengthException extends Exception {
+    public DifferentLengthException(String s){super(s);}
+}
---
+++ src/main/java/it/polimi/controller/exception/OrderException.java
@@ -0,0 +1,5 @@
+package it.polimi.controller.exception;
+
+public class OrderException extends Exception{
+    public OrderException(String s){super (s);}
+}
