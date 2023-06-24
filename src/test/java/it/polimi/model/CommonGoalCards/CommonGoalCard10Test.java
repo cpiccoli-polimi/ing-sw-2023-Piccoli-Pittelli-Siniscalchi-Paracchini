d4545c1b484b88734b8d9c5bf7cd829294ab6c10
Christian Piccoli <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-06-24 17:02:34 +0200

    Correction on CommonGoalCard10Test

--- src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard10Test.java
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard10Test.java
@@ -13,10 +13,10 @@ class CommonGoalCard10Test {
         CommonGoalCard10 cm10=new CommonGoalCard10(4);
         // Cross of 5 tiles
         bookshelf[0][0]=new PublicObjectCard(1,0,0);
-        bookshelf[1][1]=new PublicObjectCard(1,1,0);
-        bookshelf[0][2]=new PublicObjectCard(1,0,1);
-        bookshelf[2][0]=new PublicObjectCard(1,1,1);
-        bookshelf[2][2]=new PublicObjectCard(1,1,1);
+        bookshelf[1][1]=new PublicObjectCard(1,1,1);
+        bookshelf[0][2]=new PublicObjectCard(1,0,2);
+        bookshelf[2][0]=new PublicObjectCard(1,2,0);
+        bookshelf[2][2]=new PublicObjectCard(1,2,2);
 
         assertTrue(cm10.check(bookshelf));
     }
@@ -27,11 +27,11 @@ class CommonGoalCard10Test {
         bookshelf= new PublicObjectCard[6][5];
         CommonGoalCard10 cm10bis=new CommonGoalCard10(4);
         // Cross of 5 tiles
-        bookshelf[0][2]=new PublicObjectCard(1,0,0);
-        bookshelf[1][3]=new PublicObjectCard(1,1,0);
-        bookshelf[0][4]=new PublicObjectCard(1,0,1);
-        bookshelf[2][2]=new PublicObjectCard(1,1,1);
-        bookshelf[2][4]=new PublicObjectCard(1,1,1);
+        bookshelf[0][2]=new PublicObjectCard(1,0,2);
+        bookshelf[1][3]=new PublicObjectCard(1,1,3);
+        bookshelf[0][4]=new PublicObjectCard(1,0,4);
+        bookshelf[2][2]=new PublicObjectCard(1,2,2);
+        bookshelf[2][4]=new PublicObjectCard(1,2,4);
 
         assertTrue(cm10bis.check(bookshelf));
     }
