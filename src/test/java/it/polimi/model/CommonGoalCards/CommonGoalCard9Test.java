689663aa8307bcb640974351fb6ba1c8fd704846
Christian Piccoli <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-06-24 16:55:41 +0200

    Code cleanup on CommonGoalCard9Test

--- src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard9Test.java
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard9Test.java
@@ -7,41 +7,79 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CommonGoalCard9Test {
     @Test
-    void TreColonneTreTipiDiversi(){
+    void CommonGoalCard9SatisfiedThreeColumnsDifferentTypes(){
         PublicObjectCard [][] bookshelf;
-        int k=0;
         bookshelf= new PublicObjectCard[6][5];
         CommonGoalCard9 cm9=new CommonGoalCard9(4);
-        for(int i=0;i<bookshelf.length;i++){
-            bookshelf[i][0]=new PublicObjectCard(k,i,0);
-            bookshelf[i][1]=new PublicObjectCard(k+23,i,1);
-            bookshelf[i][2]=new PublicObjectCard(k+46,i,2);
-        }
+        //COLUMN 1
+        bookshelf[0][0]=new PublicObjectCard(1,0,0);
+        bookshelf[1][0]=new PublicObjectCard(23,1,0);
+        bookshelf[2][0]=new PublicObjectCard(46,2,0);
+        bookshelf[3][0]=new PublicObjectCard(1,3,0);
+        bookshelf[4][0]=new PublicObjectCard(23,4,0);
+        bookshelf[5][0]=new PublicObjectCard(46,5,0);
+        //COLUMN 2
+        bookshelf[0][3]=new PublicObjectCard(1,0,3);
+        bookshelf[1][3]=new PublicObjectCard(23,1,3);
+        bookshelf[2][3]=new PublicObjectCard(46,2,3);
+        bookshelf[3][3]=new PublicObjectCard(46,3,3);
+        bookshelf[4][3]=new PublicObjectCard(23,4,3);
+        bookshelf[5][3]=new PublicObjectCard(46,5,3);
+        //COLUMN 2
+        bookshelf[0][1]=new PublicObjectCard(1,0,1);
+        bookshelf[1][1]=new PublicObjectCard(23,1,1);
+        bookshelf[2][1]=new PublicObjectCard(23,2,1);
+        bookshelf[3][1]=new PublicObjectCard(23,3,1);
+        bookshelf[4][1]=new PublicObjectCard(23,4,1);
+        bookshelf[5][1]=new PublicObjectCard(46,5,1);
+
         assertTrue(cm9.check(bookshelf));
     }
     @Test
-    void TreColonneStessoTipo(){
+    void CommonGoalCard9SatisfiedThreeColumnsSameType(){
         PublicObjectCard [][] bookshelf;
         int k=0;
         bookshelf= new PublicObjectCard[6][5];
-        CommonGoalCard9 cm9=new CommonGoalCard9(4);
-        for(int i=0;i<bookshelf.length;i++){
-            bookshelf[i][0]=new PublicObjectCard(k,i,0);
-            bookshelf[i][1]=new PublicObjectCard(k,i,1);
-            bookshelf[i][2]=new PublicObjectCard(k,i,2);
-        }
-        assertTrue(cm9.check(bookshelf));
+        CommonGoalCard9 cm9bis=new CommonGoalCard9(4);
+        //COLUMN 1
+        bookshelf[0][0]=new PublicObjectCard(1,0,0);
+        bookshelf[1][0]=new PublicObjectCard(1,1,0);
+        bookshelf[2][0]=new PublicObjectCard(1,2,0);
+        bookshelf[3][0]=new PublicObjectCard(1,3,0);
+        bookshelf[4][0]=new PublicObjectCard(1,4,0);
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+        //COLUMN 2
+        bookshelf[0][3]=new PublicObjectCard(1,0,3);
+        bookshelf[1][3]=new PublicObjectCard(1,1,3);
+        bookshelf[2][3]=new PublicObjectCard(1,2,3);
+        bookshelf[3][3]=new PublicObjectCard(1,3,3);
+        bookshelf[4][3]=new PublicObjectCard(1,4,3);
+        bookshelf[5][3]=new PublicObjectCard(1,5,3);
+        //COLUMN 2
+        bookshelf[0][1]=new PublicObjectCard(1,0,1);
+        bookshelf[1][1]=new PublicObjectCard(1,1,1);
+        bookshelf[2][1]=new PublicObjectCard(1,2,1);
+        bookshelf[3][1]=new PublicObjectCard(1,3,1);
+        bookshelf[4][1]=new PublicObjectCard(1,4,1);
+        bookshelf[5][1]=new PublicObjectCard(1,5,1);
+
+        assertTrue(cm9bis.check(bookshelf));
     }
     @Test
-    void SoloDueColonne(){
+    void CommonGoalCard9SatisfiedTwoColumns(){
         PublicObjectCard [][] bookshelf;
         int k=0;
         bookshelf= new PublicObjectCard[6][5];
-        CommonGoalCard9 cm9=new CommonGoalCard9(4);
-        for(int i=0;i<bookshelf.length;i++){
-            bookshelf[i][0]=new PublicObjectCard(k,i,0);
-            bookshelf[i][1]=new PublicObjectCard(k,i,1);
-        }
-        assertEquals(false,cm9.check(bookshelf));
+        CommonGoalCard9 cm9tris=new CommonGoalCard9(4);
+        //COLUMN 1
+        bookshelf[0][0]=new PublicObjectCard(1,0,0);
+        bookshelf[1][0]=new PublicObjectCard(1,1,0);
+        bookshelf[2][0]=new PublicObjectCard(1,2,0);
+        //COLUMN 2
+        bookshelf[0][3]=new PublicObjectCard(1,0,3);
+        bookshelf[1][3]=new PublicObjectCard(1,1,3);
+        bookshelf[2][3]=new PublicObjectCard(1,2,3);
+
+        assertFalse(cm9tris.check(bookshelf));
     }
 }
