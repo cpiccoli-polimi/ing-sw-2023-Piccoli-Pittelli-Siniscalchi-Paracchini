ba264bfd8a30275df4ac6bf9160e4dcd23f000b3
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-25 18:23:25 +0200

    GoalCard implements Serializable

--- src/main/java/it/polimi/model/GoalCard.java
+++ src/main/java/it/polimi/model/GoalCard.java
@@ -1,6 +1,8 @@
 package it.polimi.model;
 
-public abstract class GoalCard {
+import java.io.Serializable;
+
+public abstract class GoalCard implements Serializable {
     static final long serialVersionUID = 1L;
     private Object goal;
     private Object points;
