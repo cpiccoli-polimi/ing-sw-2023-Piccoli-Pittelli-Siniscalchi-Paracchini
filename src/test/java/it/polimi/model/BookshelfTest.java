34c65bc5964f7aa1a1d42b06f58bb3530deae706
Christian Piccoli <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-06-26 18:40:54 +0200

    Completed tests on Bookshelf class

--- src/test/java/it/polimi/model/BookshelfTest.java
+++ src/test/java/it/polimi/model/BookshelfTest.java
@@ -8,6 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookshelfTest {
     @Test
+    void updateMaxDrawableObjectsTest() {
+        Bookshelf bookshelf = new Bookshelf();
+        PublicObjectCard [][] shelf;
+        shelf= new PublicObjectCard[6][5];
+        for(int i=5;i>1;i--) {
+            shelf[i][0] = new PublicObjectCard(1, i, 0);
+            bookshelf.setShelf(shelf[i][0]);
+            shelf[i][1] = new PublicObjectCard(1, i, 1);
+            bookshelf.setShelf(shelf[i][1]);
+            shelf[i][2] = new PublicObjectCard(1, i, 2);
+            bookshelf.setShelf(shelf[i][2]);
+            shelf[i][3] = new PublicObjectCard(1, i, 3);
+            bookshelf.setShelf(shelf[i][3]);
+            shelf[i][4] = new PublicObjectCard(1, i, 4);
+            bookshelf.setShelf(shelf[i][4]);
+        }
+        bookshelf.updateMaxDrawableObjects();
+        assertEquals(2,bookshelf.getMaxDrawableObjects());
+    }
+    @Test
     void getMaxDrawableObjects() {
         Bookshelf bookshelf = new Bookshelf();
         bookshelf.setMaxDrawableObjects(1);
