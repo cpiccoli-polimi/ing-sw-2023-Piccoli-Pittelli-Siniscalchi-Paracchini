cd869894977341b4c3075f84d7044e33f8a9588b
Christian Piccoli <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-06-28 10:51:26 +0200

    Fixed CommonGoalCard12 (added test cases)

--- src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard12Test.java
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard12Test.java
@@ -41,6 +41,43 @@ class CommonGoalCard12Test {
     }
 
     @Test
+    void CommonGoal12FailedTestLeftToRight() {
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard12 cm12fail=new CommonGoalCard12(4);
+
+        bookshelf[0][0]=new PublicObjectCard(1,0,0);
+        bookshelf[1][0]=new PublicObjectCard(23,1,0);
+        bookshelf[2][0]=new PublicObjectCard(23,2,0);
+        bookshelf[3][0]=new PublicObjectCard(90,3,0);
+        bookshelf[4][0]=new PublicObjectCard(90,4,0);
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+
+        bookshelf[1][1]=new PublicObjectCard(1,1,1);
+        bookshelf[2][1]=new PublicObjectCard(23,2,1);
+        bookshelf[3][1]=new PublicObjectCard(23,3,1);
+        bookshelf[4][1]=new PublicObjectCard(1,4,1);
+        bookshelf[5][1]=new PublicObjectCard(112,5,1);
+
+        bookshelf[2][2]=new PublicObjectCard(112,2,2);
+        bookshelf[3][2]=new PublicObjectCard(1,3,2);
+        bookshelf[4][2]=new PublicObjectCard(90,4,2);
+        bookshelf[5][2]=new PublicObjectCard(1,5,2);
+
+        bookshelf[3][3]=new PublicObjectCard(68,3,3);
+        bookshelf[4][3]=new PublicObjectCard(46,4,3);
+        bookshelf[5][3]=new PublicObjectCard(1,5,3);
+
+        bookshelf[4][4]=new PublicObjectCard(23,4,4);
+        bookshelf[5][4]=new PublicObjectCard(1,5,4);
+
+        //One extra tile in wrong position
+        bookshelf[3][4]=new PublicObjectCard(1,3,4);
+
+        assertFalse(cm12fail.check(bookshelf));
+    }
+
+    @Test
     void CommonGoal12SatisfiedTestLeftToRightLower() {
         PublicObjectCard[][] bookshelf;
         bookshelf= new PublicObjectCard[6][5];
@@ -67,6 +104,38 @@ class CommonGoalCard12Test {
         bookshelf[5][4]=new PublicObjectCard(1,5,4);
 
         assertTrue(cm12bis.check(bookshelf));
+    }
+
+    @Test
+    void CommonGoal12FailedTestLeftToRightLower() {
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard12 cm12bisFail=new CommonGoalCard12(4);
+
+        bookshelf[1][0]=new PublicObjectCard(23,1,0);
+        bookshelf[2][0]=new PublicObjectCard(23,2,0);
+        bookshelf[3][0]=new PublicObjectCard(90,3,0);
+        bookshelf[4][0]=new PublicObjectCard(90,4,0);
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+
+        bookshelf[2][1]=new PublicObjectCard(23,2,1);
+        bookshelf[3][1]=new PublicObjectCard(23,3,1);
+        bookshelf[4][1]=new PublicObjectCard(1,4,1);
+        bookshelf[5][1]=new PublicObjectCard(112,5,1);
+
+        bookshelf[3][2]=new PublicObjectCard(1,3,2);
+        bookshelf[4][2]=new PublicObjectCard(90,4,2);
+        bookshelf[5][2]=new PublicObjectCard(1,5,2);
+
+        bookshelf[4][3]=new PublicObjectCard(46,4,3);
+        bookshelf[5][3]=new PublicObjectCard(1,5,3);
+
+        bookshelf[5][4]=new PublicObjectCard(1,5,4);
+        
+        //One extra tile in wrong position
+        bookshelf[4][4]=new PublicObjectCard(1,4,4);
+
+        assertFalse(cm12bisFail.check(bookshelf));
     }
 
     @Test
@@ -101,5 +170,103 @@ class CommonGoalCard12Test {
         bookshelf[5][4]=new PublicObjectCard(1,5,4);
 
         assertTrue(cm12tris.check(bookshelf));
+    }
+
+    @Test
+    void CommonGoal12FailedTestRightToLeft() {
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard12 cm12trisFail=new CommonGoalCard12(4);
+
+        bookshelf[4][0]=new PublicObjectCard(23,4,0);
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+
+        bookshelf[3][1]=new PublicObjectCard(68,3,1);
+        bookshelf[4][1]=new PublicObjectCard(46,4,1);
+        bookshelf[5][1]=new PublicObjectCard(1,5,1);
+
+        bookshelf[2][2]=new PublicObjectCard(112,2,2);
+        bookshelf[3][2]=new PublicObjectCard(1,3,2);
+        bookshelf[4][2]=new PublicObjectCard(90,4,2);
+        bookshelf[5][2]=new PublicObjectCard(1,5,2);
+
+        bookshelf[1][3]=new PublicObjectCard(1,1,3);
+        bookshelf[2][3]=new PublicObjectCard(23,2,3);
+        bookshelf[3][3]=new PublicObjectCard(23,3,3);
+        bookshelf[4][3]=new PublicObjectCard(1,4,3);
+        bookshelf[5][3]=new PublicObjectCard(112,5,3);
+
+        bookshelf[0][4]=new PublicObjectCard(1,0,4);
+        bookshelf[1][4]=new PublicObjectCard(23,1,4);
+        bookshelf[2][4]=new PublicObjectCard(23,2,4);
+        bookshelf[3][4]=new PublicObjectCard(90,3,4);
+        bookshelf[4][4]=new PublicObjectCard(90,4,4);
+        bookshelf[5][4]=new PublicObjectCard(1,5,4);
+
+        //One extra tile in wrong position
+        bookshelf[2][1]=new PublicObjectCard(1,2,1);
+
+        assertFalse(cm12trisFail.check(bookshelf));
+    }
+
+    @Test
+    void CommonGoal12SatisfiedTestRightToLeftLower() {
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard12 cm12quater=new CommonGoalCard12(4);
+
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+
+        bookshelf[4][1]=new PublicObjectCard(46,4,1);
+        bookshelf[5][1]=new PublicObjectCard(1,5,1);
+
+        bookshelf[3][2]=new PublicObjectCard(1,3,2);
+        bookshelf[4][2]=new PublicObjectCard(90,4,2);
+        bookshelf[5][2]=new PublicObjectCard(1,5,2);
+
+        bookshelf[2][3]=new PublicObjectCard(23,2,3);
+        bookshelf[3][3]=new PublicObjectCard(23,3,3);
+        bookshelf[4][3]=new PublicObjectCard(1,4,3);
+        bookshelf[5][3]=new PublicObjectCard(112,5,3);
+
+        bookshelf[1][4]=new PublicObjectCard(23,1,4);
+        bookshelf[2][4]=new PublicObjectCard(23,2,4);
+        bookshelf[3][4]=new PublicObjectCard(90,3,4);
+        bookshelf[4][4]=new PublicObjectCard(90,4,4);
+        bookshelf[5][4]=new PublicObjectCard(1,5,4);
+
+        assertTrue(cm12quater.check(bookshelf));
+    }
+
+    @Test
+    void CommonGoal12FailedTestRightToLeftLower() {
+        PublicObjectCard[][] bookshelf;
+        bookshelf= new PublicObjectCard[6][5];
+        CommonGoalCard12 cm12quater=new CommonGoalCard12(4);
+
+        bookshelf[5][0]=new PublicObjectCard(1,5,0);
+
+        bookshelf[4][1]=new PublicObjectCard(46,4,1);
+        bookshelf[5][1]=new PublicObjectCard(1,5,1);
+
+        bookshelf[3][2]=new PublicObjectCard(1,3,2);
+        bookshelf[4][2]=new PublicObjectCard(90,4,2);
+        bookshelf[5][2]=new PublicObjectCard(1,5,2);
+
+        bookshelf[2][3]=new PublicObjectCard(23,2,3);
+        bookshelf[3][3]=new PublicObjectCard(23,3,3);
+        bookshelf[4][3]=new PublicObjectCard(1,4,3);
+        bookshelf[5][3]=new PublicObjectCard(112,5,3);
+
+        bookshelf[1][4]=new PublicObjectCard(23,1,4);
+        bookshelf[2][4]=new PublicObjectCard(23,2,4);
+        bookshelf[3][4]=new PublicObjectCard(90,3,4);
+        bookshelf[4][4]=new PublicObjectCard(90,4,4);
+        bookshelf[5][4]=new PublicObjectCard(1,5,4);
+
+        //One extra tile in wrong position
+        bookshelf[2][2]=new PublicObjectCard(1,2,2);
+
+        assertTrue(cm12quater.check(bookshelf));
     }
 }
