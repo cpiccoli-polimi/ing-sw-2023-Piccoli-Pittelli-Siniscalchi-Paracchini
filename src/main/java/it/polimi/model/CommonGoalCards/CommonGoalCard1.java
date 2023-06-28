f1f2f0d8accd7d8d3cb1eb613616ac638e2ca1f3
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-28 18:38:06 +0200

    Updated CommonGoalCard1

--- src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard1.java
+++ src/main/java/it/polimi/model/CommonGoalCards/CommonGoalCard1.java
@@ -3,7 +3,10 @@ package it.polimi.model.CommonGoalCards;
 import it.polimi.model.CommonGoalCard;
 import it.polimi.model.ObjectCard;
 
-public class CommonGoalCard1 extends CommonGoalCard {
+import java.util.ArrayList;
+import java.util.List;
+
+/*public class CommonGoalCard1 extends CommonGoalCard {
     public CommonGoalCard1(int playersNumber) {
         super(1);
     }
@@ -111,4 +114,48 @@ public class CommonGoalCard1 extends CommonGoalCard {
         }
         return flag;
     }
+}*/
+
+public class CommonGoalCard1 extends CommonGoalCard {
+    public CommonGoalCard1(int playersNumber) {
+        super(1);
+    }
+    public boolean check(ObjectCard[][] bookshelf) {
+        int count=0;
+        ObjectCard [][] bookshelfCopy=new ObjectCard[bookshelf.length][bookshelf[0].length];
+        for(int i=0;i <bookshelf.length;i++){
+            for(int j=0;j<bookshelf[0].length;j++){
+                bookshelfCopy[i][j]=bookshelf[i][j];
+            }
+        }
+
+        for(int r=0;r< bookshelf.length;r++){
+            for(int c=0;c<bookshelf[0].length;c++){
+                List<ObjectCard> objectList= new ArrayList<>();
+                if(bookshelfCopy[r][c]!=null) {
+                    objectList.add(bookshelfCopy[r][c]);
+                    for (int r2 = 0; r2 < bookshelf.length; r2++) {
+                        for (int c2 = 0; c2 < bookshelf[0].length; c2++) {
+                            if (bookshelfCopy[r2][c2] != null && bookshelfCopy[r2][c2].getType() != null) {
+                                if (bookshelfCopy[r2][c2].getType() == objectList.get(0).getType()) {
+                                    if ((c2 > 0 && objectList.contains(bookshelfCopy[r2][c2 - 1])) || (r2 > 0 && objectList.contains(bookshelfCopy[r2 - 1][c2]))) {
+                                        if (!objectList.contains(bookshelfCopy[r2][c2])) {
+                                            objectList.add(bookshelfCopy[r2][c2]);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                for(int k=0;k<objectList.size();k++){
+                    bookshelfCopy[objectList.get(k).getXCoordinate()][objectList.get(k).getYCoordinate()]=null;
+                }
+                if(objectList.size()>=2) count++;
+                    objectList.clear();
+                }
+            }
+        return count >= 6;
+    }
 }
+
