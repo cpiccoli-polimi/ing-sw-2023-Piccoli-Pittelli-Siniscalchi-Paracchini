4a5c0937646c806ef3655704c9188b9a6c296cdd
Lorenzo Pittelli <lorenzo.pittelli@mail.polimi.it>
2023-06-28 17:28:52 +0200

    Updated CommonGoalCard5

--- src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard5Test.java
+++ src/test/java/it/polimi/model/CommonGoalCards/CommonGoalCard5Test.java
@@ -24,9 +24,9 @@ class CommonGoalCard5Test {
         bookshelf[1][4]=new PublicObjectCard(1,1,4);
 
         bookshelf[1][0]=new PublicObjectCard(1,1,0);
-        bookshelf[2][0]=new PublicObjectCard(2,1,0);
-        bookshelf[3][0]=new PublicObjectCard(3,1,0);
-        bookshelf[4][0]=new PublicObjectCard(4,1,0);
+        bookshelf[2][0]=new PublicObjectCard(2,2,0);
+        bookshelf[3][0]=new PublicObjectCard(3,3,0);
+        bookshelf[4][0]=new PublicObjectCard(4,4,0);
 
         bookshelf[4][1]=new PublicObjectCard(1,4,1);
         bookshelf[4][2]=new PublicObjectCard(1,4,2);
@@ -47,15 +47,14 @@ class CommonGoalCard5Test {
     @Test
     void oneMissingGroup(){
         PublicObjectCard[][] bookshelf;
-        bookshelf= new PublicObjectCard[6][5];
         CommonGoalCard5 cm5=new CommonGoalCard5(4);
         bookshelf= new PublicObjectCard[6][5];
         //3 groups of 4 cards with the same type in line
 
         bookshelf[0][0]=new PublicObjectCard(1,0,0);
-        bookshelf[0][1]=new PublicObjectCard(1,0,1);
-        bookshelf[0][2]=new PublicObjectCard(1,0,2);
-        bookshelf[0][3]=new PublicObjectCard(1,0,3);
+        bookshelf[0][1]=new PublicObjectCard(2,0,1);
+        bookshelf[0][2]=new PublicObjectCard(3,0,2);
+        bookshelf[0][3]=new PublicObjectCard(4,0,3);
 
         bookshelf[1][1]=new PublicObjectCard(1,1,1);
         bookshelf[1][2]=new PublicObjectCard(1,1,2);
@@ -63,9 +62,9 @@ class CommonGoalCard5Test {
         bookshelf[1][4]=new PublicObjectCard(1,1,4);
 
         bookshelf[1][0]=new PublicObjectCard(1,1,0);
-        bookshelf[2][0]=new PublicObjectCard(25,1,0);
-        bookshelf[3][0]=new PublicObjectCard(50,1,0);
-        bookshelf[4][0]=new PublicObjectCard(100,1,0);
+        bookshelf[2][0]=new PublicObjectCard(25,2,0);
+        bookshelf[3][0]=new PublicObjectCard(50,3,0);
+        bookshelf[4][0]=new PublicObjectCard(100,4,0);
 
         bookshelf[4][1]=new PublicObjectCard(1,4,1);
         bookshelf[4][2]=new PublicObjectCard(1,4,2);
@@ -93,9 +92,9 @@ class CommonGoalCard5Test {
         bookshelf[1][4]=new PublicObjectCard(1,1,4);
 
         bookshelf[1][0]=new PublicObjectCard(1,1,0);
-        bookshelf[2][0]=new PublicObjectCard(2,1,0);
-        bookshelf[3][0]=new PublicObjectCard(3,1,0);
-        bookshelf[4][0]=new PublicObjectCard(4,1,0);
+        bookshelf[2][0]=new PublicObjectCard(2,2,0);
+        bookshelf[3][0]=new PublicObjectCard(3,3,0);
+        bookshelf[4][0]=new PublicObjectCard(4,4,0);
 
         bookshelf[4][1]=new PublicObjectCard(1,4,1);
         bookshelf[4][2]=new PublicObjectCard(1,4,2);
