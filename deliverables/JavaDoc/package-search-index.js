b78221d46d70818e91fe6efc2599d8a5b31f9c0b
Christian Piccoli <125984660+cpiccoli-polimi@users.noreply.github.com>
2023-06-30 00:04:40 +0200

    Upload of JavaDoc

---
+++ deliverables/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.polimi"},{"l":"it.polimi.client"},{"l":"it.polimi.controller"},{"l":"it.polimi.controller.exception"},{"l":"it.polimi.model"},{"l":"it.polimi.model.CommonGoalCards"},{"l":"it.polimi.model.exception"},{"l":"it.polimi.observer"},{"l":"it.polimi.server"},{"l":"it.polimi.view"}];updateSearchResults();
